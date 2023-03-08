0c1ed7d3ab36b91e571e964a3f76f11ff7e07836
abadefabio <abadefabio1@gmail.com>
2023-03-08 11:48:56 -0300

    atualizando projeto

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -125,7 +125,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('not.be.checked')
         
     })
-    it.only("seleciona um arquivo da pasta fixtures", function(){
+    it("seleciona um arquivo da pasta fixtures", function(){
         cy.get('input[type="file"]#file-upload')
         .should('not.have.value')
         .selectFile('./cypress/fixtures/example.json')
@@ -133,6 +133,35 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             console.log($input)
             expect($input[0].files[0].name).to.equal('example.json')
         })
+    })
+
+    it("seleciona um arquivo simulando um drag-and-drop", function(){
+        cy.get('input[type="file"]#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', {action: "drag-drop" })
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+        cy.fixture('example.json').as('exemploArquivo')
+        cy.get('input[type="file"]')
+        .selectFile('@exemploArquivo')
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a').should('have.attr','target', '_blank')  //atributo
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target').click()
+        
+        cy.contains('Talking About Testing').should('be.visible')
     })
 
   })
